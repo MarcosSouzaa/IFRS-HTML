c2c56fca0fe4c6a3222704a86a69927451504acb
Marcos Antonio Silva de Souza <marcossouza.git@gmail.com>
2022-02-22 19:50:51 -0300

    operações lógicas

---
+++  curso-IFRS-Javascript/operacoesAritmeticas.js
@@ -0,0 +1,26 @@
+//Operações aritméticas
+//alert(2+3);
+//vai aparecer o resultado 5 na tela html
+
+// var a = 2;
+// var b = 3;
+// alert(a+b);
+//vai aparecer o resultado 5 na tela html
+
+//var a = 12;
+//var b = 3;
+//var c = a+b;
+//alert(c);
+// vai aparecer 15 no resultado da página html.
+
+// posso usar qualquer operador:*,/,+ ou -.
+//var a = 12;
+//var b = 3;
+//var c = a/b;
+//alert(c);
+
+//Posso Também concatenar informações no alert
+var a = 12;
+var b = 3;
+var c = a+b;
+alert("O resultado da soma é: " + c);
