96523f84cf3827acca439d38711eff2463ce2c79
Marcos Antonio Silva de Souza <marcossouza.git@gmail.com>
2022-02-22 20:13:48 -0300

    prompt

---
+++ criando-alerta-concatenado.js
@@ -0,0 +1,8 @@
+
+//1- Criar uma variável para guardar o nome da pessoa.
+//2- Criar um mecanismo para meu usuário enviar o nome. Utilizamos o prompt.
+//3- Vou pegar esse nome e exibir numa mensagem para meu usuário usando o alert.
+
+var nome;
+nome = prompt("Informe o seu nome: ");
+alert("Olá, " + nome);
